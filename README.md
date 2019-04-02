42df664b973660fe9d088915b0b0e422b5d68900
cr00z <netrebinr@gmail.com>
2019-04-02 21:51:37 +0700

    make first attempt

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Script for frequency analysis of words in any text. It takes as input the path to the text file and displays in the console the ten most popular words in this file in decreasing order of frequency.
+
+# Quickstart
+
+The script requires an installed Python interpreter version 3.5, no additional packages required.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python lang_frequency.py test.txt
+frequency: 44, word: the
+frequency: 38, word: to
+frequency: 20, word: and
+frequency: 16, word: for
+frequency: 15, word: you
+frequency: 13, word: is
+frequency: 12, word: a
+frequency: 12, word: of
+frequency: 12, word: be
+frequency: 12, word: with
+
+```
+
+Use in Windows similarly.
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,36 @@
+import argparse
+import collections
+import os
 
 
-def load_data(filepath):
-    pass
+def get_cmdline_args():
+    parser = argparse.ArgumentParser(
+        description='script for frequency analysis of words in any text')
+    parser.add_argument('text_filename', metavar='text_filename', type=str,
+                        help='text filename')
+    return parser.parse_args()
 
 
-def get_most_frequent_words(text):
-    pass
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, encoding='utf-8') as text_file_object:
+        return text_file_object.read()
+
+
+def get_most_frequent_words(input_text):
+    counted_words = collections.Counter()
+    for word in input_text:
+        if word > '@':
+            counted_words[word] += 1
+    return counted_words.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    args = get_cmdline_args()
+    loaded_text = load_data(args.text_filename)
+    if loaded_text is None:
+        exit('we\'re really looking for your file to open, but didn\'t find it')
+    most_frequent_words = get_most_frequent_words(loaded_text.split())
+    for word, frequency in most_frequent_words:
+        print("frequency: {}, word: {}".format(frequency, word))
